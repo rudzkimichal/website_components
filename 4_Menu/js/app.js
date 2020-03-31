5b7a9b7071af466f810c80c840eb0b6e270a83ed
rudzkimichal <michal.rudzki@yahoo.com>
2020-03-31 03:13:00 +0200

    menu done

--- 4_Menu/js/app.js
+++ 4_Menu/js/app.js
@@ -3,7 +3,19 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
 
-  const menuFirstLevel = document.querySelectorAll('.nav ul >li');
+  const menuFirstLevel = document.querySelectorAll('.nav >ul >li');
 
+  menuFirstLevel.forEach(function(item) {
+    item.addEventListener('mouseover', function() {
+      if(this.querySelector('ul') !== null)
+        this.querySelector('ul').style.display = 'flex';
+    });
+  });
+
+  menuFirstLevel.forEach(function(item) {
+    item.addEventListener('mouseout', function() {
+      this.querySelector('ul').style.display = 'none';
+    });
+  });
 
 }
