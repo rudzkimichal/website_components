164eebc76e90bf4de2636f58e3e8fe05f027ed7b
rudzkimichal <michal.rudzki@yahoo.com>
2020-04-01 03:30:44 +0200

    tooltip

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -1,3 +1,25 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+
+  const tooltipList = document.querySelectorAll('.tooltip');
+
+  tooltipList.forEach(function(item) {
+    const tooltip = document.createElement('span');
+    
+    item.addEventListener('mouseover', function() {
+      tooltip.classList.add('tooltipText');
+      tooltip.innerText = this.getAttribute('data-text');
+      this.appendChild(tooltip);
+    });
+
+    item.addEventListener('mouseout', function() {
+      this.removeChild(tooltip);
+    });
+  });
+
+}
