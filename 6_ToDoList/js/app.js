46f44a53dda02ea17050f3e228f71d78bdca45fd
rudzkimichal <michal.rudzki@yahoo.com>
2020-04-05 18:36:21 +0200

    6_to-do list, minor changes to 2_gallery

--- 6_ToDoList/js/app.js
+++ 6_ToDoList/js/app.js
@@ -1,1 +1,67 @@
 
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+
+	const addTask = document.getElementById('addTaskButton');
+	const taskList = document.getElementById('taskList');
+	const taskInput = document.getElementById('taskInput');
+	const counter = document.getElementById('counter');
+	const removeCompleted = document.getElementById('removeFinishedTasksButton');
+	let toDoCounter = 0;
+	counter.innerText = `Remaining tasks: ${toDoCounter}`;
+
+	addTask.addEventListener('click', function() {
+
+		if(taskInput.value.length > 1 && taskInput.value.length < 100) {
+
+			const taskItem = document.createElement('li');
+
+			const taskDesc = document.createElement('h1');
+			taskDesc.innerText = taskInput.value;
+
+			const deleteBtn = document.createElement('button');
+			deleteBtn.innerText = 'Delete';
+
+			const completeBtn = document.createElement('button');
+			completeBtn.innerText = 'Complete';
+
+			taskList.appendChild(taskItem);
+			taskItem.appendChild(taskDesc);
+			taskItem.appendChild(deleteBtn);
+			taskItem.appendChild(completeBtn);
+
+			taskInput.value = null;
+			toDoCounter++;
+			counter.innerText = `Remaining tasks: ${toDoCounter}`;
+
+			deleteBtn.addEventListener('click', function() {
+				taskItem.remove();
+				toDoCounter--;
+				counter.innerText = `Remaining tasks: ${toDoCounter}`;
+			});
+
+			completeBtn.addEventListener('click', function() {
+				if(this.parentElement.classList.value.includes('done')) {
+					taskItem.classList.remove('done');
+					toDoCounter++;
+				}
+				else {
+					taskItem.classList.add('done');
+					toDoCounter--;
+				}
+
+				counter.innerText = `Remaining tasks: ${toDoCounter}`;
+			});
+		} else {
+			alert('Task description should consist of 6-99 characters.');
+		}
+	});
+
+	removeCompleted.addEventListener('click', function() {
+		taskList.querySelectorAll('li.done').forEach(item => item.remove());
+	});
+
+
+
+}
